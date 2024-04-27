7061c7b3717cc47e9aabb7e0e471e14a9dbba9e4
Dkuaik <enrq.rios.f@gmail.com>
2024-04-27 00:04:23 -0600

    resolucion de los problemas 13 y 14 de eulers problems

--- 14.longuestCollatzSequence.py
+++ 14.longuestCollatzSequence.py
@@ -2,10 +2,12 @@ import Modulos as md
 
 maxLong=1
 n=1
+collatzNum=1
 for i in range (1,1000000):
     long=len(md.collatzSequence(i))
     if maxLong<long:
         maxLong=long
-print ("La longuitud máxima es",maxLong)  
+        collatzNum=i
+print ("La longuitud máxima es",maxLong, "para el número", collatzNum)  
 
 # print(f"la secuancia es {md.collatzSequence(n)}, con longuitud {len(md.collatzSequence(n))}")
